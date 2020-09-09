d948685bb3410b102da31a441782c5f402969f16
harrison666 <296589355@qq.com>
2020-09-09 09:30:29 -0400

    finished proj1b

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,8 +1,8 @@
 public class Palindrome {
-    public Deque<Character> wordToDeque(String word){
+    public Deque<Character> wordToDeque(String word) {
         Deque<Character> result = new ArrayDeque();
         for (int i = 0; i < word.length(); i++) {
-            result.addLast(word.substring(i, i+1).charAt(0));
+            result.addLast(word.substring(i, i + 1).charAt(0));
         }
         return result;
     }
@@ -12,7 +12,8 @@ public class Palindrome {
             return true;
         }
         Deque<Character> d = wordToDeque(word);
-        return (d.removeFirst() == d.removeLast() && isPalindrome(word.substring(1, word.length() - 1)));
+        return (d.removeFirst() == d.removeLast() &&
+                isPalindrome(word.substring(1, word.length() - 1)));
         /*
         for (int i = 0; i < word.length() / 2; i++) {
             if (d.removeFirst() != d.removeLast()) {
@@ -29,6 +30,7 @@ public class Palindrome {
             return true;
         }
         Deque<Character> d = wordToDeque(word);
-        return (cc.equalChars(d.removeFirst(), d.removeLast()) && isPalindrome(word.substring(1, word.length() - 1), cc));
+        return (cc.equalChars(d.removeFirst(), d.removeLast()) &&
+                isPalindrome(word.substring(1, word.length() - 1), cc));
     }
 }
