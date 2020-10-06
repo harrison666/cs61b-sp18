a45834f6df0b5f9f984a9d2646552292fe4a60fc
harrison666 <296589355@qq.com>
2020-10-06 16:44:39 -0400

    finished lab10

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -206,8 +206,11 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
         for (int i = 1; i <= size(); i++) {
             if (contents[i].item().equals(item)) {
                 contents[i] = new Node(item, priority);
+                swim(i);
+                sink(i);
             }
         }
+
     }
 
     /**
